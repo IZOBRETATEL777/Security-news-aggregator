44aeb8af964d8fac7fe45a2ba495644707aa2db3
Roman Tolstosheyev <32099652+IZOBRETATEL777@users.noreply.github.com>
2025-03-14 10:40:20 +0400

    feat: use range for fetching

--- src/worker.ts
+++ src/worker.ts
@@ -1,12 +1,24 @@
 import { parse } from "yaml";
 import { z } from "zod";
-import { fetchTodaysRSS } from "./rss";
+import { fetchTodaysRSS, fetchRangeDateRSS, fetchRSS } from "./rss";
 import { createNewsSchema, kv, newsFactory, type News } from "./db";
 import { complete } from "./ai";
+import dayjs from 'dayjs';
 
 const yamlSchema = z.object({
     topics: z.array(z.string()).min(1),
     feeds: z.array(z.string()).min(1),
+    data_period: z.union([
+        z.object({
+            range: z.object({
+                startDate: z.string().date('Invalid start date format (YYYY-MM-DD)').optional(),
+                endDate: z.string().date('Invalid end date format (YYY-MM-DD)').optional(),
+            }),
+        }),
+        z.literal("today"),
+        z.literal("this_week")
+    ]),
+    max_articles: z.number().int().positive().optional(),
 });
 
 const configPath = "./config.yaml";
@@ -15,11 +27,31 @@ const config = yamlSchema.parse(parse(buffer));
 
 const topicsJoined = config.topics.join("\n");
 
+async function getRssFeed(url: string) {
+    const fetchPeriod = config.data_period;
+    if (fetchPeriod === "today") {
+        return fetchTodaysRSS(url);
+    } else if (typeof fetchPeriod === "object" && "range" in fetchPeriod) {
+        const { startDate, endDate } = fetchPeriod.range;
+        const start = dayjs(startDate).toDate();
+        const end = dayjs(endDate).toDate();
+        return await fetchRangeDateRSS(url, start, end);
+    }
+    else if (fetchPeriod === "this_week") {
+        const today = new Date();
+        const start = new Date(today.getFullYear(), today.getMonth(), today.getDate() - today.getDay());
+        const end = new Date(today.getFullYear(), today.getMonth(), today.getDate() + (6 - today.getDay()));
+        return await fetchRangeDateRSS(url, start, end);
+    } else {
+        return await fetchRSS(url);
+    }
+};
+
 export async function processor() {
     // Fetch news from RSS feeds to one array
     const news: News[] = [];
     for (const url of config.feeds) {
-        const rss = await fetchTodaysRSS(url);
+        const rss = await getRssFeed(url);
         for (const item of rss.items) {
             const res = createNewsSchema.safeParse(item);
 
