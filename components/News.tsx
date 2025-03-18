52907ef6ea208eb7ef134f66bbc5136f5a02cc39
Roman Tolstosheyev <32099652+IZOBRETATEL777@users.noreply.github.com>
2025-03-18 11:29:17 +0400

    feat: style keywords

--- components/News.tsx
+++ components/News.tsx
@@ -7,21 +7,28 @@ interface NewsProps {
 }
 
 export default function NewsComponent({ news }: NewsProps) {
-  return news.map(e=>({...e, published: new Date(e.published)})).map((item) => (
+  return news.map(e => ({ ...e, published: new Date(e.published) })).map((item) => (
     `<tr>
-      <td class="text-break">${item.title}</td>
-      <td class="word-break">${item.keywords.join(', ')}</td>
-      <td class="word-break">${item.published.toDateString()}</td>
-      <td class="word-break">
-        <a
-          href="${item.url}"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="btn btn-sm btn-primary"
-        >
-          Open
-        </a>
-      </td>
-    </tr>`
+    <td class="text-break">${item.title}</td>
+    <td class="word-break">
+      ${item.keywords
+      .map(
+        (keyword) =>
+          `<span class="badge rounded-pill bg-warning text-dark me-1">${keyword}</span>`
+      )
+      .join(" ")}
+    </td>
+    <td class="word-break">${item.published.toDateString()}</td>
+    <td class="word-break">
+      <a
+        href="${item.url}"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="btn btn-sm btn-primary"
+      >
+        Open
+      </a>
+    </td>
+  </tr>`
   ))
 };
