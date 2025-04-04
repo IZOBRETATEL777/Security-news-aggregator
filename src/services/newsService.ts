ce24fa6099f05f29ac5f15af7c98090e3bd5d146
Roman Tolstosheyev <32099652+IZOBRETATEL777@users.noreply.github.com>
2025-04-04 13:30:05 +0400

    chore: decouple ai templates

--- src/services/newsService.ts
+++ src/services/newsService.ts
@@ -1,30 +1,30 @@
-import { symbol } from 'zod';
 import { container } from '../configs/ioc';
 import { type BaseRepository, type News } from '../dao/db';
-import { reducedTemplate, complete } from './aiService';
+import { complete } from './aiService';
+import { reducedTemplate } from './templates/aiTemplates';
 
 export class NewsService {
     private repository = container.resolve<BaseRepository<News, string>>('repo');
 
 
-    async getNews() : Promise<News[]> {
+    async getNews(): Promise<News[]> {
         return await this.repository.find();
     }
 
-    async getNewsById(id: string) : Promise<News | null> {
+    async getNewsById(id: string): Promise<News | null> {
         return await this.repository.findById(id);
     }
 
-    async getReducedNews(topicsJoined: string, excludeTopicsJoined: string, newsItemLimit: number, aiModel: string) : Promise<News[]> {
+    async getReducedNews(topicsJoined: string, excludeTopicsJoined: string, newsItemLimit: number, aiModel: string): Promise<News[]> {
         const news = await this.getNews();
         return await complete(news, topicsJoined, excludeTopicsJoined, newsItemLimit, aiModel, reducedTemplate);
     }
 
-    async saveNews(news: News[]) : Promise<void> {
+    async saveNews(news: News[]): Promise<void> {
         await this.repository.saveNews(news);
     }
 
-    async deleteAllNews() : Promise<void> {
+    async deleteAllNews(): Promise<void> {
         await this.repository.deleteNewsIndex();
     }
 }
